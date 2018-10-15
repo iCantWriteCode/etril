0e865bf5c5ed17f53709bfde394fd26c24acb5b1
icantwritecode <andrchas@gmail.com>
2018-10-16 00:15:19 +0300

    headers & url

--- server.js
+++ server.js
@@ -9,20 +9,16 @@ const app = express();
 app.use(morgan('dev'));
 // Add headers
 app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.header('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Access-Control-Allow-Headers, Accept');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.header('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, OPTIONS, POST, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Max-Age", "3600");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, Content-Type, Authorization, X-Requested-With, Accept"
+    );
     next();
 });
 
