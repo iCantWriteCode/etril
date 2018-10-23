8a72dea891220edf9553ffdc259725f8c53606c1
icantwritecode <andrchas@gmail.com>
2018-10-24 01:36:58 +0300

    url

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 const app = angular.module('etrilco', ['ngRoute']);
 
 // const url = 'http://178.128.245.146:3002';
-const url = 'http://192.168.1.2:3002';
-// const url = 'http://etrilapi.1024dev.tk';
+// const url = 'http://192.168.1.2:3002';
+const url = 'http://etrilapi.1024dev.tk';
 
 
 app
